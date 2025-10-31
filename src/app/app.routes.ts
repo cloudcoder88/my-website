421b9c3fa62a3d66938165ac029d76b87324a728
cloudcoder88 <btech4104@gmail.com>
2025-10-31 22:33:21 +0100

    i added components to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MyServicesComponent } from './my-services/my-services.component';
+import { ContactComponent } from './contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'About',component:AboutComponent},
+    {path:'Projects',component:ProjectsComponent},
+    {path:'MyServices',component:MyServicesComponent},
+    {path:'Contact',component:ContactComponent},
+];
+
